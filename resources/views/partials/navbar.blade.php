393603ee5cfac9cc3257a13bf6d8c95cd909481b
calvintn25 <calvintn25@gmail.com>
2024-05-12 12:02:15 +0700

    commit push 3

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,28 @@
+<!-- navbar -->
+<nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" alt="Logo" width="120" height="80" class="d-inline-block align-text-top">
+      </a>
+    <ul class="nav justify-content-end">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li class="nav-item" role="presentation">
+          <a href="#home" style="text-decoration: none;"> <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button> </a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a href="#menu" style="text-decoration: none;">
+            <button class="nav-link" id="pills-menu-tab" data-bs-toggle="pill" data-bs-target="#pills-menu" type="button" role="tab" aria-controls="pills-menu" aria-selected="false">Menu</button> </a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a href="#about" style="text-decoration: none;">
+            <button class="nav-link" id="pills-About-tab" data-bs-toggle="pill" data-bs-target="#pills-about" type="button" role="tab" aria-controls="pills-About" aria-selected="false">About</button> </a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a href="#contact" style="text-decoration: none;">
+            <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button> </a>
+          </li>
+        </ul>
+      </ul>
+  </div>
+</nav>
+<!-- end of navbar-->
